94c870fbb11e32bdf6eac1f581e9aaa29eaf4f27
Nathaniel Fitzenrider <nfitzenr@unca.edu>
2024-02-18 15:24:21 -0500

    Rework backend API
    Fixes bugs and implements other changes.

--- src/deck.py
+++ src/deck.py
@@ -1,27 +1,35 @@
 from card import Card
 import random
+from dataclasses import dataclass, field
+from functools import partial
+
 SUITS = ["C", "D", "H", "S"]
 RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+CARDS = [Card(rank=r, suit=s) for r in RANKS for s in SUITS]
 
-#TODO: implement ranking
+
+# TODO: implement ranking
+@dataclass
 class Deck:
-    def __init__(self):
-        self.cards = []
-        for suit in (SUITS):
-            for rank in (RANKS):
-                card = Card(rank, suit)
-                self.cards.append(card.to_json)
-                
-        random.shuffle(self.cards)
-    
-    def pop(self):
-        return self.cards.pop()
-    
-    def draw(self, num):
-        cards_drawn = []
-        #if(self.cards.__sizeof__ < num):
-            #num = self.cards.__sizeof__
-        for _ in range(num):
-            cards_drawn.append(self.pop())
-        return cards_drawn
+    cards: list[Card] = field(
+        default_factory=partial(random.sample, CARDS, len(CARDS))
+    )
+    top: int = 0
 
+    def pop(self) -> dict[str, str]:
+        card = self.cards[self.top]
+        self.top += 1
+        return card._asdict()
+
+    def draw(self, num: int) -> list[dict[str, str]]:
+        return [self.pop() for _ in range(num)]
+
+    def to_serializable(self) -> list[dict[str, str]]:
+        return [c._asdict() for c in self.cards]
+
+    @property
+    def cards_left(self) -> int:
+        """
+        Returns the number of cards left in the deck.
+        """
+        return len(self.cards) - self.top
